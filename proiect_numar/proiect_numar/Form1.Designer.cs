8c314026c632902c67d4484fb76d3def31a5f783
Stefania1703 <116831218+Stefania1703@users.noreply.github.com>
2023-02-21 12:06:01 +0200

    numar_prim

---
+++ proiect_numar/proiect_numar/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace proiect_numar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Submitbtn = new System.Windows.Forms.Button();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Submitbtn
+            // 
+            this.Submitbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.Submitbtn.Location = new System.Drawing.Point(274, 168);
+            this.Submitbtn.Name = "Submitbtn";
+            this.Submitbtn.Size = new System.Drawing.Size(203, 71);
+            this.Submitbtn.TabIndex = 0;
+            this.Submitbtn.Text = "Submit";
+            this.Submitbtn.UseVisualStyleBackColor = false;
+            this.Submitbtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Location = new System.Drawing.Point(208, 109);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(357, 22);
+            this.numberTextBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.Submitbtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Submitbtn;
+        private System.Windows.Forms.TextBox numberTextBox;
+    }
+}
+
---
+++ proiect_numar/proiect_numar/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proiect_numar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {    
+            //MessageBox.Show("S-a introdus:" + numberTextBox.Text);
+            bool isNumber = Int32.TryParse (numberTextBox.Text, out int result);
+            if(isNumber==true)
+            {
+                bool isPrime = true;
+                for(int i=2 ; i<=result/2 ; i++)
+                {
+                    if (result % i == 0)
+                    {
+                        isPrime = false;
+                    }
+                }
+                if(isPrime==true)
+                {
+                    this.BackColor = Color.Green;
+                }
+                else
+                {
+                    this.BackColor = Color.Red;
+                }
+
+            }
+            else
+            {
+                this.BackColor=Color.Yellow;
+                MessageBox.Show("S-a introdus un text");
+                numberTextBox.Text = String.Empty;
+            }
+        }
+    }
+}
