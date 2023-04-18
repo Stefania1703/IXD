b1e4a3d44e3801c84cf614e3872d7ae60b60a723
Stefania1703 <116831218+Stefania1703@users.noreply.github.com>
2023-04-18 21:31:12 +0300

    Aplicatie

---
+++ AplicatieFreeBook/AplicatieFreeBook/SqlDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+
+namespace FreeBook
+{
+    class SqlDataAccess
+    {
+        public static string GetCartiString()
+        {
+            return ConfigurationManager.AppSettings["carti"];
+        }
+    }
+}
