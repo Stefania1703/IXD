bde86c7e76e7c264ec31ec1053df4115160d40f9
Stefania1703 <116831218+Stefania1703@users.noreply.github.com>
2023-10-30 20:58:07 +0200

    XD

---
+++ XD/XD/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XD
+{
+    public class Car
+    {
+        public string color;
+        public string name;
+        public int maxSpeed;
+        public int year;
+        public Car(string modelName, string colorName, int yearValue, int maxSpeedValue)
+        {
+            color = colorName;
+            name = modelName; 
+            maxSpeed = maxSpeedValue;
+            year = yearValue;
+
+        }
+        public Car(string modelName)
+        {
+            name = modelName;
+        }
+        public void fullThrottle()
+        {
+            Console.WriteLine(name + " is going at the speed of " + maxSpeed);
+        }
+
+    }
+}
---
+++ XD/XD/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("Mustang", "white", 2021, 100);
+            Car Opel = new Car("Astra", "blue", 2010, 120);
+            Car Volvo = new Car("XC-90");
+
+            /*Ford.name = "Mustang";
+            Ford.color = "white";
+            Ford.year = 2021;
+            Ford.maxSpeed = 100;
+
+            Opel.name = "Astra";
+            Opel.color = "blue";
+            Opel.year = 2010;
+            Opel.maxSpeed = 120;*/
+
+            Console.WriteLine(Ford.name);
+            Console.WriteLine(Ford.color);
+            Console.WriteLine(Ford.year);
+            Ford.fullThrottle();
+
+            Console.WriteLine(Opel.name);
+            Console.WriteLine(Opel.color);
+            Console.WriteLine(Opel.year);
+            Opel.fullThrottle();
+            Console.ReadKey();
+
+        }
+    }
+}
